5df324cc187d1c66200e85a17866cc54cf7f329a
renatamilanez <renatamilanez.dev@gmail.com>
2022-06-12 20:53:31 -0300

    com seleção de opções

--- scripts.js
+++ scripts.js
@@ -2,8 +2,7 @@ function selectFood (elemento) {
     const opcaoEscolhida = document.querySelector(".foods .select-option");
     if (opcaoEscolhida !== null) {
         opcaoEscolhida.classList.remove("select-option");
-    }
-
+    } 
     elemento.classList.add("select-option");
 }
 
@@ -11,8 +10,7 @@ function selectDrink (elemento) {
     const opcaoEscolhida = document.querySelector(".drinks .select-option");
     if (opcaoEscolhida !== null) {
         opcaoEscolhida.classList.remove("select-option");
-    }
-
+    } 
     elemento.classList.add("select-option");
 }
 
@@ -20,8 +18,23 @@ function selectDessert (elemento) {
     const opcaoEscolhida = document.querySelector(".desserts .select-option");
     if (opcaoEscolhida !== null) {
         opcaoEscolhida.classList.remove("select-option");
-    }
-
+    } 
     elemento.classList.add("select-option");
+}
+
+//barra verde fechar pedido
+
+function closeOrder () {
+    let confirmOrder = document.querySelector(".close-order .close-button");
+    let textOrder = document.querySelector(".close-txt");
+
+    let CheckedFood = document.querySelector(".foods .select-option")
+    let CheckedDrink = document.querySelector(".drinks .select-option")
+    let CheckedDessert = document.querySelector(".desserts .select-option")
+
+    if (CheckedFood && CheckedDrink && CheckedDessert){
+        confirmOrder.classList.add("checked-close-button");
+        textOrder.innerHTML("Fazer pedido");
+    }
 }
 
